1678ce4392fb65dd6b939b881a0221122e910868
Juan Ignacio Solerno <135361987+juanignaciosolerno@users.noreply.github.com>
2024-09-05 14:26:58 +0000

    setup authentication

--- streamlit_nefro_bot.py
+++ streamlit_nefro_bot.py
@@ -11,43 +11,55 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+openai_api_key = st.secrets["open_ai_key"]
 
+password = st.text_input("App Password", type="password")
+
+if not openai_api_key:
+    st.info("The app has not OpenAI api key.", icon="🗝️")
+else: 
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+proceed = False
+
+if not password:
+    st.info("Please add a valid password to continue.", icon="🗝️")
+else:
+    if password != st.secrets["app_password"]:
+        st.info("The provided password is incorrect.", icon="🗝️")
+    else: 
+        proceed = True
+
+# Let the user upload a file via `st.file_uploader`.
+uploaded_file = st.file_uploader(
+    "Upload a document (.txt or .md)", type=("txt", "md")
+)
+
+# Ask the user for a question via `st.text_area`.
+question = st.text_area(
+    "Now ask a question about the document!",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
+
+if uploaded_file and question:
+
+    # Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+    ]
+
+    # Generate an answer using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=True,
     )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
-
-    if uploaded_file and question:
-
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        )
-
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+    # Stream the response to the app using `st.write_stream`.
+    st.write_stream(stream)
